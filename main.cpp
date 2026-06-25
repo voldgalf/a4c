86ca6628a76ff983b0e1746448fd1cca59ca9725
brick <50calibursword@gmail.com>
2026-06-25 15:13:11 -0400

    feat: created ```AINode``` class
    Created the ```AINode``` class which is a derived class of ```Node```.

--- main.cpp
+++ main.cpp
@@ -23,38 +23,44 @@ public:
     }
 };
 
-void ai_node(json &dict, const json &properties) {
-    try {
-        const std::string properties_model = properties.value("/model_name"_json_pointer, json(nullptr));
-        const std::string properties_question_template = properties.value(
-            "/question_prompt"_json_pointer, json(nullptr));
+class AINode : public Node {
+public:
+    void run(json &dict, const json &properties) override {
+        try {
+            const std::string properties_model = properties.value("/model_name"_json_pointer, json(nullptr));
+            const std::string properties_question_template = properties.value(
+                "/question_prompt"_json_pointer, json(nullptr));
 
-        if (properties_model.empty() || properties_question_template.empty()) {
-            throw 1;
+            if (properties_model.empty() || properties_question_template.empty()) {
+                throw 1;
+            }
+
+            json llm_request_body;
+            llm_request_body["model"] = properties_model;
+            llm_request_body["input"] = inja::render(properties_question_template, dict);
+
+            http::Request llm_request{"http://10.0.0.212:1234/v1/responses"};
+            http::Response llm_response = llm_request.send("POST", llm_request_body.dump(),
+                                                           {{"Content-Type", "application/json"}});
+
+            json llm_response_body = json::parse(std::string{llm_response.body.begin(), llm_response.body.end()});
+            std::string llm_response_output = llm_response_body.value("/output/0/content/0/text"_json_pointer,
+                                                                      json(nullptr));
+            if (llm_response_output.empty()) {
+                throw 1;
+            }
+
+            dict["ai_node"]["response"] = llm_response_output;
+        } catch (std::exception &e) {
+            std::cerr << e.what() << "\n";
         }
-
-        json llm_request_body;
-        llm_request_body["model"] = properties_model;
-        llm_request_body["input"] = inja::render(properties_question_template, dict);
-
-        http::Request llm_request{"http://10.0.0.212:1234/v1/responses"};
-        http::Response llm_response = llm_request.send("POST", llm_request_body.dump(),
-                                                       {{"Content-Type", "application/json"}});
-
-        json llm_response_body = json::parse(std::string{llm_response.body.begin(), llm_response.body.end()});
-        std::string llm_response_output = llm_response_body.value("/output/0/content/0/text"_json_pointer,
-                                                                  json(nullptr));
-        if (llm_response_output.empty()) {
-            throw 1;
-        }
-
-        dict["ai_node"]["response"] = llm_response_output;
-    } catch (std::exception &e) {
-        std::cerr << e.what() << "\n";
     }
-}
+};
 
 int main() {
+    VariableNode variable_node;
+
+    AINode ai_node;
     json properties;
     properties["variable_name"] = "question_prompt";
     properties["variable_value"] = "Hello, who are you?";
@@ -63,7 +69,7 @@ int main() {
 
     std::cout << "Running Variable Node\n";
 
-    variable_node(dict, properties);
+    variable_node.run(dict, properties);
 
     std::cout << dict.dump(4) << "\n";
 
@@ -73,7 +79,7 @@ int main() {
     properties["model_name"] = "google/gemma-3-1b";
     properties["question_prompt"] = "{{ variable.question_prompt }}";
 
-    ai_node(dict, properties);
+    ai_node.run(dict, llm_properties);
 
     std::cout << dict.dump(4) << "\n";
 }
