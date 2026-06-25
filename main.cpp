897dabe617d3c03e7bd5d8788d86cee695f865b0
brick <50calibursword@gmail.com>
2026-06-25 15:42:15 -0400

    refactor: renamed ```AINode``` protected variables
    Removed all "properties_" string from protected variables

--- main.cpp
+++ main.cpp
@@ -35,21 +35,27 @@ public:
 
 class AINode : public Node {
 protected:
-    std::string properties_model;
-    std::string properties_question_template;
+    std::string model;
+    std::string question_template;
 
 public:
     explicit AINode(const json &properties) {
         properties_model = properties.value("/model_name"_json_pointer, json(nullptr));
         properties_question_template = properties.value(
             "/question_prompt"_json_pointer, json(nullptr));
+        model = properties.value("/model_name"_json_pointer, json(nullptr));
+        question_template = properties.value("/question_prompt"_json_pointer, json(nullptr));
     }
 
     void run(json &dict) override {
         try {
+            if (model.empty() || question_template.empty()) {
+                throw "Missing model parameter or question template";
+            }
+
             json llm_request_body;
-            llm_request_body["model"] = properties_model;
-            llm_request_body["input"] = inja::render(properties_question_template, dict);
+            llm_request_body["model"] = model;
+            llm_request_body["input"] = inja::render(question_template, dict);
 
             http::Request llm_request{"http://10.0.0.212:1234/v1/responses"};
             http::Response llm_response = llm_request.send("POST", llm_request_body.dump(),
