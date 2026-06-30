22392487358fbebe14610a56107b593444dd02a6
brick <50calibursword@gmail.com>
2026-06-29 20:53:46 -0400

    chore: added example Node, Node_HelloWorld
    added example node for testing purposes.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
-#include <AINode.h>
-#include <VariableNode.h>
+#include <memory>
+#include <WorkFlowManager.h>
+#include <Node.h>
+
+
+class Node_HelloWorld : public Node {
+protected:
+    std::string variable_template;
+    std::string output_variable;
+
+public:
+    Node_HelloWorld() = default;
+
+    explicit Node_HelloWorld(const inja::json &properties);
+
+    void run(inja::json &state) override {
+        is_executed = true;
+
+
+        std::cout << "Hello World!\n";
+    };
+};
+
+/*
+void AINode::run(inja::json &state) {
+    try {
+        if (model.empty() || prompt.empty()) {
+            throw std::runtime_error("Missing model name or prompt");
+        }
+
+        inja::json llm_request_body;
+        llm_request_body["model"] = model;
+        llm_request_body["input"] = inja::render(prompt, state);
+
+        http::Request llm_request{"http://10.0.0.212:1234/v1/responses"};
+        http::Response llm_response = llm_request.send("POST", llm_request_body.dump(),
+                                                       {{"Content-Type", "application/json"}},
+                                                       std::chrono::milliseconds(7000));
+
+        inja::json llm_response_body = inja::json::parse(
+            std::string{llm_response.body.begin(), llm_response.body.end()});
+        std::string llm_response_output = llm_response_body.value("/output/0/content/0/text"_json_pointer,
+                                                                  "");
+        if (llm_response_output.empty()) {
+            throw std::runtime_error("Invalid LLM Response");
+        }
+
+        state["ai_node"]["response"] = llm_response_output;
+    } catch (std::exception &e) {
+        std::cerr << e.what() << "\n";
+    }
+}
+*/
 
 int main() {
+    /*
     VariableNode variable_node({
         {"variable_name", "prompt"},
         {"variable_value", "Hello, who are you?"},
@@ -12,19 +64,13 @@ int main() {
         {"model_name", "google/gemma-3-1b"},
         {"prompt", "{{ variable.prompt }}"},
     });
+    */
 
-    inja::json state = {};
+    WorkflowManager manager = WorkflowManager();
 
-    std::cout << "Running Variable Node\n";
+    std::unique_ptr<Node> hello_world = std::make_unique<Node_HelloWorld>();
 
-    variable_node.run(state);
+    manager.add_node(std::move(hello_world));
 
-    std::cout << state.dump(4) << "\n";
-
-    std::cout << "Running AI Node\n";
-
-
-    ai_node.run(state);
-
-    std::cout << state.dump(4) << "\n";
+    manager.run_all();
 }
