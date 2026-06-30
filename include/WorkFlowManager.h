4c585fda11ff3af11699296f344ccc3fcb52a08b
brick <50calibursword@gmail.com>
2026-06-29 20:52:23 -0400

    feat: created WorkFlowManager class
    This class handles node execution and state.

---
+++ include/WorkFlowManager.h
@@ -0,0 +1,23 @@
+//
+// Created by brick on 6/29/2026.
+//
+#include <memory>
+#include <Node.h>
+#pragma once
+
+
+class WorkflowManager {
+protected:
+    std::vector<std::unique_ptr<Node> > nodes;
+    int current_node = 0;
+    inja::json state = {};
+
+public:
+    WorkflowManager();
+
+    void add_node(std::unique_ptr<Node> node);
+
+    void run_next();
+
+    void run_all();
+};
---
+++ src/WorkFlowManager.cpp
@@ -0,0 +1,31 @@
+//
+// Created by brick on 6/29/2026.
+//
+
+#include "../include/WorkFlowManager.h"
+
+WorkflowManager::WorkflowManager() {
+    current_node = 0;
+}
+
+void WorkflowManager::add_node(std::unique_ptr<Node> node) {
+    nodes.push_back(std::move(node));
+}
+
+void WorkflowManager::run_next() {
+    std::unique_ptr<Node> &node = nodes[current_node];
+
+    if (!node->is_executed) {
+        node->run(state);
+
+        if (current_node < nodes.size()) {
+            current_node += 1;
+        }
+    }
+}
+
+void WorkflowManager::run_all() {
+    for (std::unique_ptr<Node> &node: nodes) {
+        node->run(state);
+    }
+}
